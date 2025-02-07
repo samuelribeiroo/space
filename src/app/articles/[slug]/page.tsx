c4bc88fdced54a9b465592d3a4b5bff865fbb81a
samuelribeiroo <samuelribeirodev@gmail.com>
2025-02-06 21:47:53 -0300

    refactor(remove warn): params by context

--- src/app/articles/[slug]/page.tsx
+++ src/app/articles/[slug]/page.tsx
@@ -3,18 +3,25 @@ import {
   ArticleContent,
   ArticleMain,
 } from "@/app/components/ui/article-ui";
-import { getPostBySlug } from "@/libs/contentful";
+import { getPostBySlug, getPosts } from "@/libs/contentful";
 import { formatContentfulPost } from "@/libs/contentful/utils";
 import { publishDateFormatter } from "@/utils/date-formatter";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 import { notFound } from "next/navigation";
+import React from "react";
 
-export default async function Article({
-  params,
-}: {
-  params: { slug: string };
-}) {
-  const getPostEntry = await getPostBySlug(params.slug);
+
+type SingleArticleProps = {
+  context: { params: { slug: string } }
+}
+
+export default async function Article(context: { params: {  slug: string } }) {
+
+  // 'await' has no effect on the type of this expression. -> but dont remove. If remove will see a log warning
+  /* Route "/articles/[slug]" used `params.slug`. `params` should be awaited before using its properties */
+  const { slug } = await context.params;
+
+  const getPostEntry = await getPostBySlug(slug);
 
   if (!getPostEntry) notFound();
 
