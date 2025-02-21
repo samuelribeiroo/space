668cb81ada67f7c963563d31ef6522791f92f0ae
samuelribeiroo <samuelribeirodev@gmail.com>
2025-02-20 22:41:35 -0300

    update(components): including and refactor elements

--- src/app/components/ui/projects-ui.tsx
+++ src/app/components/ui/projects-ui.tsx
@@ -1,12 +1,9 @@
 import { PropsWithChildren, ReactNode } from "react";
-import { Badge } from "./badge";
 
 function Project({ children }: PropsWithChildren) {
   return (
     <>
-      <main className="space-y-16 mt-16 container mx-auto px-4">
-        {children}
-      </main>
+      <main className="space-y-16 mt-20 container px-4">{children}</main>
     </>
   );
 }
@@ -37,29 +34,31 @@ function BadgeIcons({ content, children }: BadgeIconsProps) {
 function ProjectImage(props: { url: string; alt: string }) {
   return (
     <>
-      <div className="relative aspect-[4/3] rounded-lg overflow-hidden [@media(max-width:900px)]:mx-4">
-        <img src={props.url} alt={props.alt} className="object-cover size-full cursor-pointer" />
+      <div className="relative aspect-[4/3] rounded-lg overflow-hidden">
+        <img
+          src={props.url}
+          alt={props.alt}
+          className="object-cover size-full cursor-pointer"
+        />
       </div>
     </>
   );
 }
 
 function ProjectTitle(props: { title: string }) {
-  return (
-    <>
-      <h3 className="text-2xl font-bold">{props.title}</h3>
-    </>
-  );
+  return <h3 className="text-2xl font-bold">{props.title}</h3>;
 }
 
 /*  details bc contain description */
-
-function ProjectDetails(props: { company: string, description: string | undefined }) {
+function ProjectDetails(props: {
+  classficiation: string;
+  description: string | undefined;
+}) {
   return (
     <>
       <p className="text-sm text-gray-400">
-        Made at{" "}
-        <span className="font-medium text-white">{props.company}</span>
+        {/* Made at{" "} */}
+        <span className="font-medium text-white">{props.classficiation}</span>
       </p>
 
       <p className="text-gray-300">{props.description}</p>
@@ -86,12 +85,86 @@ function ProjectContent({ title, children }: ProjectContentProps) {
 function TechStack({ children }: PropsWithChildren) {
   return (
     <>
-    <div className="flex flex-wrap gap-2">
-      {children}
-    </div>
+      <div className="flex flex-wrap gap-2 select-none">{children}</div>
     </>
-  )
+  );
+}
+
+/* Project Grid UI Elements  */
+function ProjectGridContiner({ children }: PropsWithChildren) {
+  return (
+    <>
+      <section className="container mx-auto mt-32">{children}</section>
+    </>
+  );
+}
+
+function BentoGrid({ children }: PropsWithChildren) {
+  return (
+    <>
+      <span className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        {children}
+      </span>
+    </>
+  );
+}
+
+function ProjectGridContent(props: { title: string; description: string }) {
+  return (
+    <>
+      <h4 className="text-zinc-200 text-xl font-semibold mb-2">
+        {props.title}
+      </h4>
+      <p className="text-gray-200">{props.description}</p>
+    </>
+  );
+}
+
+/* Project Highlight UI Elements   */
+
+type HeroProps = PropsWithChildren<{
+  heading: string;
+  content: string;
+}>;
+
+function Hero({
+  children,
+  heading,
+  content,
+}: HeroProps) {
+  return (
+    <>
+      <section className="max-w-7xl mx-auto px-4">
+      <div className="space-y-6">
+        {children}
+
+        <div className="flex flex-col justify-center">
+          <h1 className="text-4xl sm:text-5xl lg:text-6xl font-bold tracking-[0.5px] leading-tight mb-8">
+            {heading}
+          </h1>
+
+          <p className="font-karla text-lg sm:text-xl text-gray-400 lg:text-right">
+            {content}
+            <br />
+          </p>
+        </div>
+      </div>
+      </section>
+    </>
+  );
 }
 
 
-export { Project, Container, BadgeIcons, ProjectImage, TechStack, ProjectDetails, ProjectContent };
+export {
+  Project,
+  Container,
+  BadgeIcons,
+  ProjectImage,
+  TechStack,
+  ProjectDetails,
+  ProjectContent,
+  ProjectGridContiner,
+  BentoGrid,
+  ProjectGridContent,
+  Hero
+};
